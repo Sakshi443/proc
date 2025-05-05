f6ec18e50eeb26bf44d7f887a4f02177a17509e2
Sakshi Manoorkar <126005443+Sakshi443@users.noreply.github.com>
2025-05-05 08:59:54 +0000

    commit

--- public/profDashboard/professorDashboard.js
+++ public/profDashboard/professorDashboard.js
@@ -1,9 +1,8 @@
 function logout() {
-    alert("Logging out...");
-    window.location.href = "/proc/public/index.html"; // Optional
-  }
-  
-  function goTo(page) {
-    window.location.href = page;
-  }
-  
+  alert("Logging out...");
+  window.location.href = "/proc/public/index.html"; // Optional
+}
+
+function goTo(page) {
+  window.location.href = page;
+}
