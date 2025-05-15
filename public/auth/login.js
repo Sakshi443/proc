feed4c04bae113d94dd87757adee2e69b828f792
Sakshi Manoorkar <126005443+Sakshi443@users.noreply.github.com>
2025-05-15 16:27:49 +0000

    commit

--- public/auth/login.js
+++ public/auth/login.js
@@ -1,29 +1,29 @@
-import { auth } from "./firebase.js";
-import {
-  signInWithEmailAndPassword,
-} from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
+// import { auth } from "./firebase.js";
+// import {
+//   signInWithEmailAndPassword,
+// } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
 
-document.getElementById("loginForm").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const email = document.getElementById("loginEmail").value;
-  const password = document.getElementById("loginPassword").value;
+// document.getElementById("loginForm").addEventListener("submit", async (e) => {
+//   e.preventDefault();
+//   const email = document.getElementById("loginEmail").value;
+//   const password = document.getElementById("loginPassword").value;
 
-  try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
+//   try {
+//     const userCredential = await signInWithEmailAndPassword(auth, email, password);
+//     const user = userCredential.user;
 
-    const role = user.displayName;
+//     const role = user.displayName;
 
-    if (role === "student") {
-      alert("Verify face (simulated)");
-      // Optional: Add face recognition logic
-      window.location.href = "./dashboard/studentDashboard.html";
-    } else if (role === "professor") {
-      window.location.href = "./dashboard/professorDashboard.html";
-    } else {
-      alert("Invalid user role");
-    }
-  } catch (error) {
-    alert(error.message);
-  }
-});
+//     if (role === "student") {
+//       alert("Verify face (simulated)");
+//       // Optional: Add face recognition logic
+//       window.location.href = "./dashboard/studentDashboard.html";
+//     } else if (role === "professor") {
+//       window.location.href = "./dashboard/professorDashboard.html";
+//     } else {
+//       alert("Invalid user role");
+//     }
+//   } catch (error) {
+//     alert(error.message);
+//   }
+// });
