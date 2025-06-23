f40648a64d938d87c802c4f653fa8102a6ec9a8e
Sakshi Manoorkar <126005443+Sakshi443@users.noreply.github.com>
2025-06-23 19:57:42 +0000

    commit changes

--- firebase.js
+++ firebase.js
@@ -1,16 +1,11 @@
 // firebase.js
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
   apiKey: "AIzaSyAg-Qc46CzCYdN_JGayHuR7xYxlsryUpZc",
   authDomain: "proctored-system.firebaseapp.com",
